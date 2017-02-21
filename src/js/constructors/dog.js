11d87b18e8d3af2f0dcabf8647987b4443b9364d
Marie Filbey <patriciamariefilbey@gmail.com>
2017-02-21 16:16:30 -0500

    Normal mode passing.

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,4 +1,16 @@
-function Dog () {
-}
+function Dog (options) {
+  var options = options || {};
+  this.status = 'normal' || options.status;
+  this.color = options.color;
+
+  //moonshine doesn't have a hungry option
+  if (options.hungry === undefined) {
+      this.hungry = true;
+    } else {
+      this.hungry = options.hungry;
+  }
+ }
+
+
 
 export { Dog };
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,4 +1,19 @@
-function Person () {
+function Person (options) {
+  var options = options || {};
+
+ if (options.cool === undefined) {
+    this.cool = false;
+  } else {
+    this.cool = options.cool;
+  }
+}
+
+Person.prototype.pet = function (dog) {
+  dog.status = "happy";
+};
+
+Person.prototype.feed = function (dog) {
+  dog.hungry = false;
 };
 
 export { Person };
